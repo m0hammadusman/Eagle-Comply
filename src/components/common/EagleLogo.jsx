fd959b78adc0f9da21d7e9a1bc87e65cd43f8c33
Muhammad Usman <drusmanka00786@gmail.com>
2026-08-19 15:06:21 +0500

    implement EagleLogo component and logic

---
+++ src/components/common/EagleLogo.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useTheme } from '../../context/ThemeContext';
+
+export default function EagleLogo({ 
+  className = 'h-10 md:h-12 lg:h-14 xl:h-16 w-auto',
+  alt = 'Eagle Compliance',
+  forceWhite = false
+}) {
+  const { isDark } = useTheme();
+
+  if (forceWhite) {
+    return (
+      <div className="relative inline-flex items-center justify-center shrink-0">
+        <img
+          src="/logo-dark.png"
+          alt={alt}
+          className={`${className} object-contain shrink-0`}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative inline-flex items-center justify-center shrink-0">
+      {/* Light Mode Logo */}
+      <img
+        src="/logo-light.png"
+        alt={alt}
+        className={`${className} object-contain transition-opacity duration-300 block dark:hidden shrink-0`}
+      />
+      {/* Dark Mode Logo */}
+      <img
+        src="/logo-dark.png"
+        alt={alt}
+        className={`${className} object-contain transition-opacity duration-300 hidden dark:block shrink-0`}
+      />
+    </div>
+  );
+}
